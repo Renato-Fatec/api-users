9df733547b820c6944c439bb151457e1f75d7da7
Renato-Fatec <151967186+Renato-Fatec@users.noreply.github.com>
2024-09-02 23:41:57 +0000

    Rodando

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-
 const app = express();
-const port = 3001;
+const port = 3000;
+
+require('dotenv').config()
 
 // Configuração do MySQL
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root', // Seu nome de usuário do MySQL
-  password: '08960896', // Sua senha do MySQL
-  database: 'usersdb'
+  user: process.env.MYSQL_USER, // Seu nome de usuário do MySQL
+  password: process.env.MYSQL_PASSWORD, // Sua senha do MySQL
+  database: process.env.MYSQL_DATABASE
 });
 
 // Conectar ao banco de dados
